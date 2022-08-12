1b028a0be9cf2d853fae3428456abbcd09e22665
zhengweize <735876803@qq.com>
2022-08-12 12:24:29 +0800

    feat: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
+    title: 'GXW-UI',
     base: '/gxw-ui/',
     themeConfig: {
+        nav: [
+            {
+                text: 'Home', link: '/'
+            },
+            {
+                text: "GitHub", link: 'https://github.com/Zheng-weize/gxw-ui'
+            },
+            {
+                text: "Npm", link: "https://www.npmjs.com/package/gxw-component-ui"
+            }
+        ],
         sidebar: [
             '/',
             '/componentsDocs/card'
